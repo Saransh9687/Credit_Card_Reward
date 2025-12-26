48dad49fb2926a89a11fd63efdb68a325412bf5c
Arpitha141 <arpitha.ramachandra141@gmail.com>
2025-12-26 11:45:39 +0000

    Added Dependencies and Structure for Backend

---
+++ Credit-Card-Reward-Redemption/src/test/java/com/credit/rewards/CreditCardRewardRedemptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardRewardRedemptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
